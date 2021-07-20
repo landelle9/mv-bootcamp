d70eda8afdc60b85e721cb4cfb8e411f1408bc05
Leah Andelle <leah.a.williams1@gmail.com>
2021-07-20 11:41:15 +0100

    Latest changes

---
+++ Web servers/insert.js
@@ -0,0 +1,52 @@
+const { Restaurant } = require("./Models/Restaurant");
+const { Menu } = require("./Models/Menu");
+const { MenuItem } = require("./Models/MenuItem");
+const fsp = require("fs").promises; // Node.js file system module with promises
+const { sequelize } = require("./sequelize-connect");
+
+/**
+ * Load the data from the file and
+ * insert it in the database
+ */
+async function loadAndInsert() {
+	const restaurantFile = "./restaurants.json"; //asynchronously reads the contents of the restaurant data file
+	const buffer = await fsp.readFile(restaurantFile); // wait for the restaurant data file to be read
+	const restaurantsArray = JSON.parse(String(buffer)); //converts the read file data into JS objects (arrays)
+
+	// recreate the database tables
+	await sequelize.sync({ force: true });
+
+	let menuCounter = 1;
+
+	for (let i = 0; i < restaurantsArray.length; i++) {
+		const currentRestaurant = restaurantsArray[i];
+
+		await Restaurant.create({
+			name: currentRestaurant.name,
+			image: currentRestaurant.image,
+		});
+
+		for (let j = 0; j < currentRestaurant.menus.length; j++) {
+			const currentMenu = currentRestaurant.menus[j];
+
+			await Menu.create({ title: currentMenu.title, restaurant_id: i + 1 });
+
+			for (let k = 0; k < currentMenu.items.length; k++) {
+				const currentMenuItem = currentMenu.items[k];
+
+				await MenuItem.create({
+					name: currentMenuItem.name,
+					price: currentMenuItem.price,
+					menu_id: menuCounter,
+				});
+			}
+
+			menuCounter++;
+		}
+	}
+}
+
+module.export = loadAndInsert;
+
+// local testing
+loadAndInsert();
--- Web servers/server.js
+++ Web servers/server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+	console.log(`Server listening at http://localhost:${port}`);
 });
 
 Restaurant.hasMany(Menu);
@@ -27,33 +27,200 @@ Menu.hasMany(MenuItem);
 MenuItem.belongsTo(Menu);
 
 app.get("/now", (request, response) => {
-  const date = new Date();
-  response.send(date);
+	const date = new Date();
+	response.send(date);
 });
 
 app.get("/flipcoin", (request, response) => {
-  if (port == 3000) {
-    response.send("heads");
-  } else {
-    response.send("tails");
-  }
+	if (port == 3000) {
+		response.send("heads");
+	} else {
+		response.send("tails");
+	}
 });
 //READ
-app.get("/restaurants", (request, response) => {
-  response.send("All restos");
+app.get("/restaurants", async (request, response) => {
+	const restaurants = await Restaurant.findAll();
+	response.send(restaurants);
 });
+
+app.get("/restaurants/:id", async (request, response) => {
+	const restaurantid = request.params.id;
+	const restaurant = await Restaurant.findByPk(restaurantid, {
+		include: [Menu], // This ensures that when searching for restaurants, that menu is joined/associated with it.
+	});
+	console.log(restaurant);
+
+	if (restaurant === null) {
+		response.sendStatus(404);
+	} else {
+		response.send(restaurant);
+	}
+});
+
+// If there is no restaurant in the database, a 404 error will be sent back.
+
 //CREATE
 app.post("/restaurants", async (request, response) => {
-  console.log(request.body); // Similiar to sending a letter, and needing to include an envelope, letter etc.
+	console.log(request.body); // Similiar to sending a letter, and needing to include an envelope, letter etc.
 
-  await Restaurant.create(request.body);
-  response.sendStatus(201);
+	await Restaurant.create({
+		name: request.body.name,
+		image: request.body.image,
+	});
+	response.sendStatus(201);
+	response.send("Restaurant created");
+});
+
+//UPDATE
+app.put("/restaurants/:id", async (request, response) => {
+	console.log(request.params.id);
+	console.log(request.body);
+
+	await Restaurant.update(
+		{
+			name: request.body.name,
+			image: request.body.image,
+		},
+		{
+			where: {
+				id: request.params.id,
+			},
+		}
+	);
+	response.send("Put resto");
+});
+
+//DELETE
+app.delete("/restaurants/:id", async (request, response) => {
+	await Restaurant.destroy({
+		where: {
+			id: request.params.id,
+		},
+	});
+	response.send("Delete resto");
+});
+
+//READ
+app.get("/menus", async (request, response) => {
+	const menu = await Menu.findAll();
+	response.send(menu);
+});
+
+app.get("/menus/:id", async (request, response) => {
+	const menuid = request.params.id;
+	const menu = await Menu.findByPk(menuid, {
+		include: [Restaurant],
+	});
+
+	console.log(Menu);
+
+	if (menu === null) {
+		response.sendStatus(404);
+	} else {
+		response.send(menu);
+	}
+});
+//CREATE
+app.post("/menus", async (request, response) => {
+	console.log(request.body); // Similiar to sending a letter, and needing to include an envelope, letter etc.
+
+	await Menu.create({
+		RestaurantId: request.body.restaurantid,
+		name: request.body.name,
+		image: request.body.image,
+	});
+	response.sendStatus(201);
+	response.send("Menu created");
 });
 //UPDATE
-app.put("/restaurants", (request, response) => {
-  response.send("Put resto");
+app.put("/menus/:id", async (request, response) => {
+	console.log(request.params.id);
+	console.log(request.body);
+
+	await Menu.update(
+		{
+			name: request.body.name,
+			image: request.body.image,
+		},
+		{
+			where: {
+				id: request.params.id,
+			},
+		}
+	);
+	response.send("Put Menu");
 });
+
 //DELETE
-app.delete("/restaurants", (request, response) => {
-  response.send("Delete resto");
+app.delete("/menus/:id", async (request, response) => {
+	await Menu.destroy({
+		where: {
+			id: request.params.id,
+		},
+	});
+	response.send("Delete menu");
+});
+
+//READ
+app.get("/menuitems", async (request, response) => {
+	const menuitems = await MenuItem.findAll();
+	response.send(menuitemsid, {
+		include: [Menu],
+	});
+});
+
+app.get("/menuitems/:id", async (request, response) => {
+	const menuitemsid = request.params.id;
+	const menuitems = await MenuItem.findByPk(menuitemsid, {
+		include: [Menu],
+	});
+	console.log(Menu);
+
+	if (menuitems === null) {
+		response.sendStatus(404);
+	} else {
+		response.send(menuitems);
+	}
+});
+//CREATE
+app.post("/menuitems", async (request, response) => {
+	console.log(request.body); // Similiar to sending a letter, and needing to include an envelope, letter etc.
+
+	await MenuItem.create({
+		MenuId: request.body.menuid,
+		name: request.body.name,
+		price: request.body.price,
+	});
+	response.sendStatus(201);
+	response.send("Menu item created");
+});
+
+//UPDATE
+app.put("/menuitems/:id", async (request, response) => {
+	console.log(request.params.id);
+	console.log(request.body);
+
+	await MenuItem.update(
+		{
+			name: request.body.name,
+			price: request.body.price,
+		},
+		{
+			where: {
+				id: request.params.id,
+			},
+		}
+	);
+	response.send("Put Menu Item");
+});
+
+//DELETE
+app.delete("/menuitems/:id", async (request, response) => {
+	await MenuItem.destroy({
+		where: {
+			id: request.params.id,
+		},
+	});
+	response.send("Delete menu item");
 });
